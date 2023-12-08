91efef5e13d683105e83022beee3bf817f11662e
TaeBbong <mok05289@korea.ac.kr>
2023-12-08 19:33:16 +0900

    feat: viewmodel base structure

--- lib/viewmodels/result_viewmodel.dart
+++ lib/viewmodels/result_viewmodel.dart
@@ -2,6 +2,8 @@ import 'package:get/get_state_manager/get_state_manager.dart';
 
 class ResultViewModel extends GetxController {
   // String scanResult; ...
+  double probability = -1;
+  List<String> samples = [];
 
   void getScanResult() {}
   void cleanModel() async {}
